fbb4dc613444a38a7e8502d869ddaf0e3209f412
SantiagoMartin001 <SantiagoMartin001@users.noreply.github.com>
2023-07-11 19:52:20 -0300

    Agregar archivos de proyecto.

---
+++ primerParcialsnm1.Windows/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace primerParcialsnm1.Windows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtLado = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.altura = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ladoBase = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.area = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.superficie = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese la altura de la piramide";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese el lado de la piramide";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(281, 43);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 22);
+            this.txtAltura.TabIndex = 2;
+            // 
+            // txtLado
+            // 
+            this.txtLado.Location = new System.Drawing.Point(281, 93);
+            this.txtLado.Name = "txtLado";
+            this.txtLado.Size = new System.Drawing.Size(100, 22);
+            this.txtLado.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.altura,
+            this.ladoBase,
+            this.area,
+            this.superficie});
+            this.dataGridView1.Location = new System.Drawing.Point(29, 144);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(625, 269);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // altura
+            // 
+            this.altura.HeaderText = "altura";
+            this.altura.MinimumWidth = 6;
+            this.altura.Name = "altura";
+            this.altura.ReadOnly = true;
+            this.altura.Width = 125;
+            // 
+            // ladoBase
+            // 
+            this.ladoBase.HeaderText = "lado base";
+            this.ladoBase.MinimumWidth = 6;
+            this.ladoBase.Name = "ladoBase";
+            this.ladoBase.ReadOnly = true;
+            this.ladoBase.Width = 125;
+            // 
+            // area
+            // 
+            this.area.HeaderText = "area";
+            this.area.MinimumWidth = 6;
+            this.area.Name = "area";
+            this.area.ReadOnly = true;
+            this.area.Width = 125;
+            // 
+            // superficie
+            // 
+            this.superficie.HeaderText = "Superficie";
+            this.superficie.MinimumWidth = 6;
+            this.superficie.Name = "superficie";
+            this.superficie.ReadOnly = true;
+            this.superficie.Width = 125;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(397, 42);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(125, 73);
+            this.btnOk.TabIndex = 5;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(545, 43);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(109, 72);
+            this.btnCancelar.TabIndex = 6;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtLado);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Calculadora Piramides";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtLado;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn altura;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ladoBase;
+        private System.Windows.Forms.DataGridViewTextBoxColumn area;
+        private System.Windows.Forms.DataGridViewTextBoxColumn superficie;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ primerParciasnm1.Consola/Program.cs
@@ -0,0 +1,33 @@
+﻿double area, altura, volumen, ladoBase;
+int contadorPiramides;
+
+contadorPiramides = 0;
+try
+{
+
+
+do
+{
+    Console.WriteLine("Ingrese la altura de la pirámide: (0 Para salir)");
+    altura = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese la base de la pirámide: (0 Para salir)");
+    ladoBase = double.Parse(Console.ReadLine());
+
+    area = ladoBase * (ladoBase + Math.Sqrt(4 * Math.Pow(altura, 2) + Math.Pow(ladoBase, 2)));
+    volumen = (Math.Pow(ladoBase, 2) * altura) / 3;
+    contadorPiramides = contadorPiramides + 1;
+    Console.Write("El área total de la pirámide es: " + area);
+    Console.Write("El volumen de la pirámide es: " + volumen);
+
+} while (ladoBase != 0 && altura != 0);
+
+}
+catch (Exception)
+{
+
+    Console.WriteLine("Valor ingresado no es válido");
+}
+Console.ReadLine();
+
+Console.WriteLine(" Se ingresaron " + contadorPiramides + " pirámides");
